99e50d643a24e65e1bc994488cc7d79e10fb3f8f
Kay-ddggxh <kathrin.welfare@gmail.com>
2024-02-12 20:33:52 +0000

    install and connect allauth

--- woohoo_haiku/settings.py
+++ woohoo_haiku/settings.py
@@ -45,12 +45,25 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites',
+
+    # Allauth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    # Other
     "crispy_forms",
     "crispy_bootstrap4",
     "bootstrap4",
+
     # Apps
     "haikus",
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
@@ -63,6 +76,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = "woohoo_haiku.urls"
@@ -120,6 +134,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- woohoo_haiku/urls.py
+++ woohoo_haiku/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
     path("", include("haikus.urls"), name="haikus-urls"),
 ]
